12a81869df89764c0421f7f96ef8ee20b99831a1
petch <tanee.kumpijit@gmail.com>
2018-04-10 11:02:01 +0700

    add todo

--- src/main/idp.js
+++ src/main/idp.js
@@ -10,5 +10,7 @@ var privKey = 'IDP_PrivateKey';
 */
 export async function createIdpResponse(data) {
   data.signature = utils.createSignature(privKey,JSON.stringify(data));
-  return await utils.updateChain('CreateIdpResponse',data,utils.getNonce());
+  let result = await utils.updateChain('CreateIdpResponse',data,utils.getNonce());
+  //send msg to rp
+  return result;
 }
