2bb1b98a74409dcc6210c6d4341148ea5de62daf
Raphassit Leelachaikul <raphassitlee@gmail.com>
2018-04-10 12:12:26 +0700

    Fix multiple connect on same socket message queue

--- src/msq/index.js
+++ src/msq/index.js
@@ -2,13 +2,12 @@ import EventEmitter from 'events';
 import zmq from 'zeromq';
 
 const receivingSocket = zmq.socket('pull');
-const sendingSocket = zmq.socket('push');
 
 export const eventEmitter = new EventEmitter();
 
 receivingSocket.on('message', function(jsonMessageStr){
   // TODO - Receiving Queue
-  // In case of messages arrive before listener function is added
+  // In case of messages arrive before listener function is added.
 
   const message = JSON.parse(jsonMessageStr);
 
@@ -18,8 +17,14 @@ receivingSocket.on('message', function(jsonMessageStr){
 export const send = (receivers, message) => {
   const jsonMessageStr = JSON.stringify(message);
   receivers.forEach(receiver => {
+    const sendingSocket = zmq.socket('push');
     sendingSocket.connect(`tcp://${receiver.ip}:${receiver.port}`);
     sendingSocket.send(jsonMessageStr);
+
+    // TO BE REVISED
+    // When should we disconnect the socket?
+    // If the socket is disconnected, all the messages in queue will be lost. 
+    // Hence, the receiver won't get the messages.
     sendingSocket.disconnect(`tcp://${receiver.ip}:${receiver.port}`);
   });
 };
