058209813a41450953ea39218f211f99169180f9
Raphassit Leelachaikul <raphassitlee@gmail.com>
2018-05-14 10:23:08 +0700

    Add DB functions

--- src/db/index.js
+++ src/db/index.js
@@ -5,30 +5,60 @@ import db from './sequelize';
 //
 
 export function getRequestIdsExpectedInBlock(fromHeight, toHeight) {
-  return db.getListRange('requestIdsExpectedInBlock', {
-    greaterThanOrEqual: fromHeight,
-    lessThanOrEqual: toHeight,
+  return db.getListRange({
+    name: 'requestIdExpectedInBlock',
+    keyName: 'expectedBlockHeight',
+    keyRange: {
+      gte: fromHeight, // greaterThanOrEqual
+      lte: toHeight, // lessThanOrEqual
+    },
   });
 }
 
 export function addRequestIdExpectedInBlock(height, requestId) {
-  return db.pushToList('requestIdsExpectedInBlock', height, requestId);
+  return db.pushToList({
+    name: 'requestIdExpectedInBlock',
+    keyName: 'expectedBlockHeight',
+    key: height,
+    valueName: 'requestId',
+    value: requestId,
+  });
 }
 
 export function removeRequestIdsExpectedInBlock(height, requestIds) {
-  return db.removeFromList('requestIdsExpectedInBlock', height, requestIds);
+  return db.removeFromList({
+    name: 'requestIdExpectedInBlock',
+    keyName: 'expectedBlockHeight',
+    key: height,
+    valueName: 'requestId',
+    valuesToRemove: requestIds,
+  });
 }
 
 export function getRequestReceivedFromMQ(requestId) {
-  return db.get('requestReceivedFromMQ', requestId);
+  return db.get({
+    name: 'requestReceivedFromMQ',
+    keyName: 'requestId',
+    key: requestId,
+  });
 }
 
 export function setRequestReceivedFromMQ(requestId, request) {
-  return db.set('requestReceivedFromMQ', requestId, request);
+  return db.set({
+    name: 'requestReceivedFromMQ',
+    keyName: 'requestId',
+    key: requestId,
+    valueName: 'request',
+    value: request,
+  });
 }
 
 export function removeRequestReceivedFromMQ(requestId) {
-  return db.remove('requestReceivedFromMQ', requestId);
+  return db.remove({
+    name: 'requestReceivedFromMQ',
+    keyName: 'requestId',
+    key: requestId,
+  });
 }
 
 //
@@ -36,53 +66,111 @@ export function removeRequestReceivedFromMQ(requestId) {
 //
 
 export function getRequestIdByReferenceId(referenceId) {
-  return db.get('requestIdReferenceIdMapping', referenceId);
+  return db.get({
+    name: 'requestIdReferenceIdMapping',
+    keyName: 'referenceId',
+    key: referenceId,
+  });
 }
 
 export function setRequestIdByReferenceId(referenceId, requestId) {
-  return db.set('requestIdReferenceIdMapping', referenceId, requestId);
+  return db.set({
+    name: 'requestIdReferenceIdMapping',
+    keyName: 'referenceId',
+    key: referenceId,
+    valueName: 'requestId',
+    value: requestId,
+  });
 }
 
 export function removeRequestIdByReferenceId(referenceId) {
-  return db.remove('requestIdReferenceIdMapping', referenceId);
+  return db.remove({
+    name: 'requestIdReferenceIdMapping',
+    keyName: 'referenceId',
+    key: referenceId,
+  });
 }
 
 export function getRequestToSendToAS(requestId) {
-  return db.get('requestToSendToAS', requestId);
+  return db.get({
+    name: 'requestToSendToAS',
+    keyName: 'requestId',
+    key: requestId,
+  });
 }
 
 export function setRequestToSendToAS(requestId, request) {
-  return db.set('requestToSendToAS', requestId, request);
+  return db.set({
+    name: 'requestToSendToAS',
+    keyName: 'requestId',
+    key: requestId,
+    valueName: 'request',
+    value: request,
+  });
 }
 
 export function removeRequestToSendToAS(requestId) {
-  return db.remove('requestToSendToAS', requestId);
+  return db.remove({
+    name: 'requestToSendToAS',
+    keyName: 'requestId',
+    key: requestId,
+  });
 }
 
 export function getCallbackUrls(requestId) {
-  return db.get('callbackUrls', requestId);
+  return db.get({
+    name: 'callbackUrl',
+    keyName: 'requestId',
+    key: requestId,
+  });
 }
 
 export function setCallbackUrl(requestId, callbackUrl) {
-  return db.set('callbackUrls', requestId, callbackUrl);
+  return db.set({
+    name: 'callbackUrl',
+    keyName: 'requestId',
+    key: requestId,
+    valueName: 'url',
+    value: callbackUrl,
+  });
 }
 
 export function removeCallbackUrl(requestId) {
-  return db.remove('callbackUrls', requestId);
+  return db.remove({
+    name: 'callbackUrl',
+    keyName: 'requestId',
+    key: requestId,
+  });
 }
 
 export function getDatafromAS(requestId) {
-  return db.getList('dataFromAS', requestId);
+  return db.getList({
+    name: 'dataFromAS',
+    keyName: 'requestId',
+    key: requestId,
+  });
 }
 
 export function addDataFromAS(requestId, data) {
-  return db.pushToList('dataFromAS', requestId, data);
+  return db.pushToList({
+    name: 'dataFromAS',
+    keyName: 'requestId',
+    key: requestId,
+    valueKey: 'data',
+    value: data,
+  });
 }
 
 export function removeDataFromAS(requestId) {
-  return db.removeFromList('dataFromAS', requestId);
+  return db.removeList({
+    name: 'dataFromAS',
+    keyName: 'requestId',
+    key: requestId,
+  });
 }
 
 export function removeAllDataFromAS() {
-  return db.removeList('dataFromAS');
+  return db.removeAllLists({
+    name: 'dataFromAS',
+  });
 }
--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -1,37 +1,130 @@
 import path from 'path';
+import Sequelize from 'sequelize';
 
 import * as config from '../config';
 
-const dbPath = path.join(__dirname, `../../db-api-${config.nodeId}`);
+const dbPath = path.join(
+  __dirname,
+  '..',
+  '..',
+  `db-api-${config.nodeId}.sqlite`
+);
 
-export function getList(dataName, key) {
+const sequelize = new Sequelize('ndid-api', null, null, {
+  dialect: 'sqlite',
+  storage: dbPath,
+});
 
+// Models
+const Entities = {
+  requestIdExpectedInBlock: sequelize.define('requestIdExpectedInBlock', {
+    requestId: Sequelize.STRING,
+    expectedBlockHeight: Sequelize.INTEGER,
+  }),
+  requestReceivedFromMQ: sequelize.define('requestReceivedFromMQ', {
+    requestId: { type: Sequelize.STRING, primaryKey: true },
+    request: Sequelize.JSON,
+  }),
+  requestIdReferenceIdMapping: sequelize.define('requestIdReferenceIdMapping', {
+    referenceId: { type: Sequelize.TEXT, primaryKey: true },
+    requestId: { type: Sequelize.STRING, unique: true },
+  }),
+  requestToSendToAS: sequelize.define('requestToSendToAS', {
+    requestId: { type: Sequelize.STRING, primaryKey: true },
+    request: Sequelize.JSON,
+  }),
+  callbackUrl: sequelize.define('callbackUrl', {
+    requestId: { type: Sequelize.STRING, primaryKey: true },
+    url: Sequelize.TEXT,
+  }),
+  dataFromAS: sequelize.define('dataFromAS', {
+    requestId: Sequelize.STRING,
+    asNodeId: Sequelize.STRING,
+    data: Sequelize.JSON,
+  }),
+};
+
+sequelize.sync();
+
+export async function getList({ name, keyName, key }) {
+  const models = await Entities[name].findAll({
+    where: {
+      [keyName]: key,
+    },
+  });
+  return models.map((model) => model.get({ plain: true }));
 }
 
-export function getListRange(dataName, keyRange) {
-
+export async function getListRange({ name, keyName, keyRange }) {
+  const models = await Entities[name].findAll({
+    where: {
+      [keyName]: {
+        [Sequelize.Op.gte]: keyRange.gte,
+        [Sequelize.Op.lte]: keyRange.lte,
+      },
+    },
+  });
+  return models.map((model) => model.get({ plain: true }));
 }
 
-export function pushToList(dataName, key, value) {
-
+export async function pushToList({ name, keyName, key, valueName, value }) {
+  await Entities[name].create({
+    [keyName]: key,
+    [valueName]: value,
+  });
 }
 
-export function removeFromList(dataName, key, valuesToRemove) {
-
+export async function removeFromList({
+  name,
+  keyName,
+  key,
+  valueName,
+  valuesToRemove,
+}) {
+  await Entities[name].destroy({
+    where: {
+      [keyName]: key,
+      [valueName]: {
+        [Sequelize.Op.in]: valuesToRemove,
+      },
+    },
+  });
 }
 
-export function removeList(dataName, key) {
-
+export async function removeList({ name, keyName, key }) {
+  await Entities[name].destroy({
+    where: {
+      [keyName]: key,
+    },
+  });
 }
 
-export function get(dataName, key) {
-
+export async function removeAllLists({ name }) {
+  await Entities[name].destroy({
+    where: {},
+  });
 }
 
-export function set(dataName, key, value) {
-
+export async function get({ name, keyName, key }) {
+  const model = await Entities[name].findOne({
+    where: {
+      [keyName]: key,
+    },
+  });
+  return model != null ? model.get({ plain: true }) : null;
 }
 
-export function remove(dataName, key) {
+export async function set({ name, keyName, key, valueName, value }) {
+  await Entities[name].upsert({
+    [keyName]: key,
+    [valueName]: value,
+  });
+}
 
+export async function remove({ name, keyName, key, valueName, value }) {
+  await Entities[name].destroy({
+    where: {
+      [keyName]: key,
+    },
+  });
 }
