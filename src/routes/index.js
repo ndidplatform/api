8fcda96961e54f8ea6b0db4829584dc647c8a9e0
Raphassit Leelachaikul <raphassitlee@gmail.com>
2018-06-21 11:08:36 +0700

    Set content type for license to text/plain, Move /license and /source to upper most route to avoid server ready middleware

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,25 +1,27 @@
 /**
  * Copyright (c) 2018, 2019 National Digital ID COMPANY LIMITED
- * 
+ *
  * This file is part of NDID software.
- * 
+ *
  * NDID is the free software: you can redistribute it and/or modify it under
  * the terms of the Affero GNU General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or any later
  * version.
- * 
+ *
  * NDID is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the Affero GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the Affero GNU General Public License
  * along with the NDID source code. If not, see https://www.gnu.org/licenses/agpl.txt.
- * 
+ *
  * Please contact info@ndid.co.th for any further questions
- * 
+ *
  */
 
+import path from 'path';
+import fs from 'fs';
 import express from 'express';
 
 import errorType from '../error/type';
@@ -38,7 +40,6 @@ import getInfo from './info';
 import * as tendermint from '../tendermint';
 
 import * as config from '../config';
-import path from 'path';
 
 const router = express.Router();
 
@@ -68,6 +69,18 @@ if (config.env === 'development') {
     next();
   });
 }
+
+router.get('/license', (req, res) => {
+  const licenseText = fs.readFileSync(
+    path.join(__dirname, '..', '..', 'COPYING')
+  );
+  res.set('Content-Type', 'text/plain');
+  res.status(200).send(licenseText);
+});
+
+router.get('/source', (req, res) => {
+  res.status(200).send('https://github.com/ndidplatform/api');
+});
 
 router.use((req, res, next) => {
   // Reject all requests when tendermint is not yet ready.
@@ -111,14 +124,6 @@ router.use('/dpki', dpkiRouter);
 router.get('/info', getInfo);
 
 router.use(errorHandler);
-
-router.get('/license', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', '..', 'COPYING'));
-});
-
-router.get('/source', (req, res) => {
-  res.status(200).send('https://github.com/ndidplatform/api');
-});
 
 // All other paths besides stated above are invalid
 router.get('*', function(req, res) {
