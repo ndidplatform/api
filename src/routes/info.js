a97792409b5f094a5ed096010a2fd2e7ad35a746
Raphassit Leelachaikul <raphassitlee@gmail.com>
2018-06-21 14:51:37 +0700

    Always get node info when calling /info

--- src/routes/info.js
+++ src/routes/info.js
@@ -1,31 +1,31 @@
 /**
  * Copyright (c) 2018, 2019 National Digital ID COMPANY LIMITED
- * 
+ *
  * This file is part of NDID software.
- * 
+ *
  * NDID is the free software: you can redistribute it and/or modify it under
  * the terms of the Affero GNU General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or any later
  * version.
- * 
+ *
  * NDID is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the Affero GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the Affero GNU General Public License
  * along with the NDID source code. If not, see https://www.gnu.org/licenses/agpl.txt.
- * 
+ *
  * Please contact info@ndid.co.th for any further questions
- * 
+ *
  */
 
 import * as tendermintNdid from '../tendermint/ndid';
 import * as config from '../config';
 
-let nodeInfo;
-
 export default async function getInfo(req, res, next) {
+  const nodeInfo = await tendermintNdid.getNodeInfo(config.nodeId);
+
   res.status(200).json({
     env: config.env,
     version: null, // FIXME: read from file?
@@ -40,9 +40,3 @@ export default async function getInfo(req, res, next) {
     tendermintAddress: config.tendermintAddress,
   });
 }
-
-async function getInfoFromBlockchain() {
-  nodeInfo = await tendermintNdid.getNodeInfo(config.nodeId);
-}
-
-getInfoFromBlockchain();
