f23b64067a9b5605358d007e9e2d3c620533ca62
Raphassit Leelachaikul <raphassitlee@gmail.com>
2018-07-09 17:43:24 +0700

    Fix dev https key directory path

--- src/config.js
+++ src/config.js
@@ -35,11 +35,11 @@ export const https = process.env.HTTPS === 'true';
 export const httpsKeyPath =
   process.env.HTTPS_KEY_PATH != null
     ? process.env.HTTPS_KEY_PATH
-    : path.join(__dirname, '..', 'devHttpsKey', 'key.pem');
+    : path.join(__dirname, '..', 'dev_https_key', 'key.pem');
 export const httpsCertPath =
   process.env.HTTPS_CERT_PATH != null
     ? process.env.HTTPS_CERT_PATH
-    : path.join(__dirname, '..', 'devHttpsKey', 'cert.pem');
+    : path.join(__dirname, '..', 'dev_https_key', 'cert.pem');
 
 export const clientHttpErrorCode = process.env.CLIENT_HTTP_ERROR_CODE || 400;
 export const serverHttpErrorCode = process.env.SERVER_HTTP_ERROR_CODE || 500;
