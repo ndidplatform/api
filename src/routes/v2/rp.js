01a503931e241972a2ec403521e369742be176d2
Raphassit Leelachaikul <raphassitlee@gmail.com>
2018-08-03 15:06:28 +0700

    Fix missing property from return body 'initial_salt' of POST /rp/requests/:namespace/:identifier

--- src/routes/v2/rp.js
+++ src/routes/v2/rp.js
@@ -70,7 +70,7 @@ router.post(
         request_timeout,
       } = req.body;
 
-      const { request_id, request_message_salt } = await common.createRequest(
+      const result = await common.createRequest(
         {
           mode,
           namespace,
@@ -88,7 +88,7 @@ router.post(
         { synchronous: false }
       );
 
-      res.status(202).json({ request_id, request_message_salt });
+      res.status(202).json(result);
     } catch (error) {
       next(error);
     }
