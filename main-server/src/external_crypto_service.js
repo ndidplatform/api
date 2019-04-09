c8407c94d7871709c52fa1514105d3b82cdace0f
Raphassit Leelachaikul <raphassitlee@gmail.com>
2019-04-09 12:03:20 +0700

    Fix incorrect 'request_message' for external crypto service sign test

--- main-server/src/external_crypto_service.js
+++ main-server/src/external_crypto_service.js
@@ -77,7 +77,7 @@ export async function checkCallbackUrls() {
 async function testSignCallback(url, publicKey, isMaster) {
   const body = {
     node_id: config.nodeId,
-    request_message: TEST_MESSAGE,
+    request_message: TEST_MESSAGE_BASE_64,
     request_message_hash: hash(TEST_MESSAGE),
     hash_method: 'SHA256',
     key_type: 'RSA',
