969f12829dd4a8b541c36e7487322313a42d2a8d
Raphassit Leelachaikul <raphassitlee@gmail.com>
2021-03-23 12:04:04 +0700

    Update install deps script

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -5,11 +5,21 @@ rm -rf node_modules
 npm install
 cd ..
 
+cd ndid-error
+rm -rf node_modules
+npm install
+cd ..
+
 cd main-server
 rm -rf node_modules
 npm install
 cd ..
 
 cd mq-server
+rm -rf node_modules
+npm install
+cd ..
+
+cd telemetry
 rm -rf node_modules
 npm install
